799ef8a7bef6e58325efdee4ef95283c7f55a2d7
Subrata <bubai@outlook.in>
2024-12-28 01:04:24 +0530

    vuejs added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "astro/config";
-
 import windi from "astro-windicss";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [windi()],
+  integrations: [windi(), vue()],
   vite: {
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    }
   },
-  server: { host: "0.0.0.0", port: 3000 },
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  }
 });
